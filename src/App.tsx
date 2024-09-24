af4822e16eb58161360615827c0dccb41d2d5a3b
systic <llsysticll@gmail.com>
2024-09-24 20:01:29 +0900

    Recap

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,26 @@ const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <H1>{text}</H1>;
+}
+
 function App() {
+  const [value, setValue] = useState("");
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setValue("value");
+  }
   return (
     <Container>
-      <H1>Protected!</H1>
+      <Dummy active text="Hello" />
+      <H1>{value}</H1>
+      <button onClick={onClick}>click me</button>
     </Container>
   );
 }
