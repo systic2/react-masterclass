653b7d44a20bc56958cbf4315a7b267f200bc440
systic <llsysticll@gmail.com>
2024-09-25 20:05:43 +0900

    createBrowserRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./screens/Home";
 import About from "./screens/About";
+import Root from "./Root";
 
-function Router() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-export default Router;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+export default router;
