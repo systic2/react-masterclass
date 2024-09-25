fd03a55651e958b5cfe4fe1cfeb8c5807c0ec38e
systic <llsysticll@gmail.com>
2024-09-25 19:53:28 +0900

    BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import { useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-interface DummyProps {
-  text: string;
-  active?: boolean;
-}
-
-function Dummy({ text, active = false }: DummyProps) {
-  return <H1>{text}</H1>;
-}
+import React from "react";
+import Router from "./Router";
 
 function App() {
-  const [value, setValue] = useState("");
-  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    setValue("value");
-  }
   return (
-    <Container>
-      <Dummy active text="Hello" />
-      <H1>{value}</H1>
-      <button onClick={onClick}>click me</button>
-    </Container>
+    <div>
+      <Router />
+    </div>
   );
 }
 
