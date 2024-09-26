ca06e5c27bd8ef14f3680211a031c546b15f2f31
systic <llsysticll@gmail.com>
2024-09-26 20:00:42 +0900

    Outlet

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ import Root from "./Root";
 import NotFound from "./screens/NotFound";
 import ErrorComponent from "./components/ErrorComponent";
 import User from "./screens/users/User";
+import Followers from './screens/users/Followers';
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,13 @@ const router = createBrowserRouter([
       {
         path: "users/:userId",
         element: <User />,
+        children: [
+          {
+            path: "followers",
+            element: <Followers />,
+            
+          }
+        ]
       },
     ],
     errorElement: <NotFound />,
