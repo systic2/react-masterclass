ad151f0b0259abbe434190bbd96996be988d852e
systic <llsysticll@gmail.com>
2024-10-15 19:08:02 +0900

    React Query part One

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router";
-import { ThemeProvider } from 'styled-components';
-import { theme } from './theme';
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
