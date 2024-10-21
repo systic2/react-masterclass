6a34c85d920b31c17c5ab4d9f43601bbfd4209e1
systic <llsysticll@gmail.com>
2024-10-21 20:00:39 +0900

    Price Chart part Two

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,7 @@
-const BASE_URL = `https://api.coinpaprika.com/v1/`;
+const BASE_URL = `https://api.coinpaprika.com/v1`;
 
 export function fetchCoins() {
-  return fetch(`${BASE_URL}/coins`, { credentials: "omit" }).then(
-    (response) => response.json()
-  );
+  return fetch(`${BASE_URL}/coins`).then((response) => response.json());
 }
 
 export function fetchCoinInfo(coinId: string) {
--- src/screens/Chart.tsx
+++ src/screens/Chart.tsx
@@ -1,16 +1,79 @@
-import { useQuery } from 'react-query';
-import { useOutletContext } from 'react-router-dom';
-import { fetchCoinHistory } from '../api';
+import { useQuery } from "react-query";
+import { useOutletContext } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
   coinId: string;
 }
 
+interface IHistroical {
+  time_open: number;
+  time_close: number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+}
+
 function Chart() {
   const { coinId } = useOutletContext<ChartProps>();
-  const {isLoading, data} = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+  const { isLoading, data } = useQuery<IHistroical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
   console.log(data);
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+              width: 5,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              labels: {
+                show: false,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
