11e7e092a27e3d5382b6686115c9f850e69b3596
systic <llsysticll@gmail.com>
2024-11-04 19:45:01 +0900

    Set Selectors

--- src/Root.tsx
+++ src/Root.tsx
@@ -76,9 +76,12 @@ const GlobalStyle = createGlobalStyle`
 
 function Root() {
   const [minutes, setMinutes] = useRecoilState(minutesState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
   };
   return (
     <>
@@ -90,7 +93,12 @@ function Root() {
           type="number"
           placeholder="Minutes"
         />
-        <input value={hours} type="number" placeholder="Hours" />
+        <input
+          value={hours}
+          onChange={onHoursChange}
+          type="number"
+          placeholder="Hours"
+        />
       </div>
       <Outlet />
     </>
