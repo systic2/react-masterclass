dbeffee710476360bdeb13148716abf419582904
systic <llsysticll@gmail.com>
2024-11-22 20:43:48 +0900

    Final Project part One

--- src/Root.tsx
+++ src/Root.tsx
@@ -85,9 +85,19 @@ const Wrapper = styled(motion.div)`
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   display: flex;
@@ -96,30 +106,39 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function Root() {
   const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => setClicked((prev) => !prev);
+  const toggle = () => setClicked((prev) => !prev);
   return (
     <>
       <GlobalStyle />
-      <Wrapper onClick={toggleClicked}>
-        <Box>
-          {!clicked ? (
-            <Circle layoutId="circle" style={{ borderRadius: 50 }} />
-          ) : null}
-        </Box>
-        <Box>
+      <Wrapper onClick={toggle}>
+        <Grid>
+          <Box layoutId="hello" />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+        <AnimatePresence>
           {clicked ? (
-            <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+            <Overlay
+              initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+              animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+              exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+            >
+              <Box layoutId="hello" style={{ width: 400, height: 200 }} />
+            </Overlay>
           ) : null}
-        </Box>
+        </AnimatePresence>
       </Wrapper>
       <Outlet />
     </>
